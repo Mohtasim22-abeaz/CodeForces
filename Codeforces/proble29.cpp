fff92411a46989ff66ac66655eb2d5ee73d87b47
Mohtasim22-abeaz <abeazdihan@gmail.com>
2025-04-11 16:35:11 +0600

    Add files via upload

---
+++ Codeforces/proble29.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char next,c;
+    cin>>c;
+    if(c=='z'){
+        next='a';
+        cout<<next<<endl;
+    }
+    else{
+        next=c+1;
+        cout<<next<<endl;
+        }
+
+    return 0;
+}
